9475231e806ac2d77879c499f1952d55c111a2ad
meili19916 <315179118@qq.com>
2021-03-19 15:44:13 +0800

    bug

--- HLJZhongkeControl/Classes/VC/LedControllManager/HLJLedControllManager.m
+++ HLJZhongkeControl/Classes/VC/LedControllManager/HLJLedControllManager.m
@@ -42,12 +42,12 @@
 
 - (void)openLed{
     NSString *openCmd = @"FF FF FF FF FF FF 00 00 00 00 78 34 01 00 21 00 00 00 00 00 00 00 00 00 00 AF 90 A5";
-    [self.controlSocket writeData:[self dataWithHexString:openCmd] withTimeout:-1 tag:0];
+    [self.controlSocket writeData:[NSKeyedArchiver archivedDataWithRootObject:[self hexToByteArray:openCmd]] withTimeout:-1 tag:0];
 }
 
 -(void)closeLed{
     NSString *closeCmd = @"FF FF FF FF FF FF 00 00 00 00 78 34 01 00 22 00 00 00 00 00 00 00 00 00 00 AF 90 A5";
-    [self.controlSocket writeData:[self dataWithHexString:closeCmd] withTimeout:-1 tag:0];
+    [self.controlSocket writeData:[NSKeyedArchiver archivedDataWithRootObject:[self hexToByteArray:closeCmd]] withTimeout:-1 tag:0];
 }
 
 
@@ -210,32 +210,4 @@
     return result;
 }
 
--(NSData*)dataWithHexString:(NSString*)str{
-    
-    if (!str || [str length] == 0) {
-        return nil;
-    }
-    
-    NSMutableData *hexData = [[NSMutableData alloc] initWithCapacity:8];
-    NSRange range;
-    if ([str length] % 2 == 0) {
-        range = NSMakeRange(0, 2);
-    } else {
-        range = NSMakeRange(0, 1);
-    }
-    for (NSInteger i = range.location; i < [str length]; i += 2) {
-        unsigned int anInt;
-        NSString *hexCharStr = [str substringWithRange:range];
-        NSScanner *scanner = [[NSScanner alloc] initWithString:hexCharStr];
-        
-        [scanner scanHexInt:&anInt];
-        NSData *entity = [[NSData alloc] initWithBytes:&anInt length:1];
-        [hexData appendData:entity];
-        
-        range.location += range.length;
-        range.length = 2;
-    }
-    return hexData;
-    
-}
 @end
